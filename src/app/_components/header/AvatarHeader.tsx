12087a831e9b22bbccd25b6776338a9ee9648b65
Alex Barr <alexbarrbusiness@gmail.com>
2025-05-20 21:35:29 +1000

    feat: added back header content and checked responsiveness

--- src/app/_components/header/AvatarHeader.tsx
+++ src/app/_components/header/AvatarHeader.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import avatar from "~/assets/avatar.jpg";
 
-interface Props {}
-
-const AvatarHeader: React.FC<Props> = ({}) => {
+const AvatarHeader: React.FC = ({}) => {
   return (
     <div className="absolute bottom-[-7.5rem] left-1/2 -translate-x-1/2 transform md:bottom-[-9rem] lg:bottom-[-10rem] xl:bottom-[-12rem]">
       <Avatar className="h-48 w-48 border-green-700 bg-white shadow-lg md:h-64 md:w-64 lg:h-72 lg:w-72 xl:h-84 xl:w-84">
