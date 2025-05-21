d9bccaf3898df0336f2cd3dcd9318d7785aecc2f
Alex Barr <alexbarrbusiness@gmail.com>
2025-05-21 12:05:35 +1000

    fix: removed depreciated react syntax

--- src/app/_components/header/ProfileHeaderContent.tsx
+++ src/app/_components/header/ProfileHeaderContent.tsx
@@ -3,7 +3,7 @@
 export default function ProfileHeaderContent() {
   return (
     <div className="relative flex h-full items-center justify-center text-center">
-      <h1 className="text-3xl font-bold text-black sm:text-4xl md:text-5xl lg:text-6xl ">
+      <h1 className="text-3xl font-bold text-black sm:text-4xl md:text-5xl lg:text-6xl">
         Alex Barr
         <br className="mb-2" />
         <span className="mt-4 block">I'm a Full Stack Developer</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 // app/page.tsx
 
-
 import Navbar from "./_components/Navbar";
 import AnimatedHeader from "./_components/header/AnimatedHeader";
 import Body from "./_components/body/Body";
@@ -11,7 +10,7 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="min-h-screen min-w-screen overflow-hidden bg-gray-50 text-gray-900 ">
+    <main className="min-h-screen min-w-screen overflow-hidden bg-gray-50 text-gray-900">
       <Navbar />
       <div className="flex flex-col space-y-20 md:space-y-40 lg:space-y-60">
         <AnimatedHeader />
