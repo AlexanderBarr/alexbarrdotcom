e790ed54940bfb9b2392d63798e4bd9a2ba642be
Alex Barr <alexbarrbusiness@gmail.com>
2025-05-21 12:17:05 +1000

    fix: lint issue

--- src/components/header/ProfileHeaderContent.tsx
+++ src/components/header/ProfileHeaderContent.tsx
@@ -6,7 +6,7 @@ export default function ProfileHeaderContent() {
       <h1 className="text-3xl font-bold text-black sm:text-4xl md:text-5xl lg:text-6xl">
         Alex Barr
         <br className="mb-2" />
-        <span className="mt-4 block">I'm a Full Stack Developer</span>
+        <span className="mt-4 block">I&apos;m a Full Stack Developer</span>
       </h1>
     </div>
   );
